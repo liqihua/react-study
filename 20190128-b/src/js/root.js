8ad822da95754807d724df7e64aae956a4a4a048
liqihua <iamliqihua@qq.com>
2019-01-28 17:42:17 +0800

    liqihua

--- 20190128-b/src/js/root.js
+++ 20190128-b/src/js/root.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory} from 'react-router';
 
+
 export default class Root extends React.Component{
     render(){
         return (
             <div>root init</div>
-    );
+        );
     };
 }
 
---
+++ 20190128-b/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: "./src/js/root.js",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['react','es2015'],
+                        plugins: ['react-html-attrs']
+                    }
+                }
+            },{
+                test: /\.css$/,
+                use: {
+                    loader: 'style-loader!css-loader'
+                }
+            }
+        ]
+    },
+    output: {
+        path: __dirname,
+        filename: './src/bundle.js'
+    }
+};
