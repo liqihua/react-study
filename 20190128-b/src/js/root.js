82f1bada1901e1aaa9c2aeddc1edcba61bda4dad
liqihua <iamliqihua@qq.com>
2019-01-29 20:28:42 +0800

    liqihua

--- 20190128-b/src/js/root.js
+++ 20190128-b/src/js/root.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router,Route,hashHistory} from 'react-router';
+import {Button} from 'antd';
+import 'antd/dist/antd.css';
 
 
 export default class Root extends React.Component{
     render(){
         return (
-            <div>root init</div>
+            <div>
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+            </div>
         );
     };
 }
--- 20190128-b/webpack.config.js
+++ 20190128-b/webpack.config.js
@@ -17,9 +17,7 @@ module.exports = {
                 }
             },{
                 test: /\.css$/,
-                use: {
-                    loader: 'style-loader!css-loader'
-                }
+                use: ['style-loader','css-loader']
             }
         ]
     },
